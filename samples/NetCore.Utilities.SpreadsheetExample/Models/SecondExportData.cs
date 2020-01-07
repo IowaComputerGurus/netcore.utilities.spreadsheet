19c3ad8ee3b1701c0ea774b7787d4a7e655a8f18
Mitchel Sellers <msellers@iowacomputergurus.com>
2020-01-07 13:49:32 -0600

    Creation of feature for multiple sheet export

---
+++ samples/NetCore.Utilities.SpreadsheetExample/Models/SecondExportData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCore.Utilities.SpreadsheetExample.Models
+{
+    class SecondExportData
+    {
+    }
+}
--- src/NetCore.Utilities.Spreadsheet/EPPlusSpreadsheetGenerator.cs
+++ src/NetCore.Utilities.Spreadsheet/EPPlusSpreadsheetGenerator.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Reflection;
 using OfficeOpenXml;
 
 namespace ICG.NetCore.Utilities.Spreadsheet
@@ -11,6 +10,98 @@ namespace ICG.NetCore.Utilities.Spreadsheet
     /// </summary>
     public class EPPlusSpreadsheetGenerator: ISpreadsheetGenerator
     {
+
+        /// <inheritdoc />
+        public byte[] CreateMultiSheetSpreadsheet(IEnumerable<ISpreadsheetConfiguration<object>> exportSheets)
+        {
+            //Validate inputs
+            if (exportSheets == null)
+                throw new ArgumentNullException(nameof(exportSheets));
+
+
+            //Create the package and render each sheet
+            using (var package = new ExcelPackage())
+            {
+                foreach (var exportConfiguration in exportSheets)
+                {
+                    var sheet = package.Workbook.Worksheets.Add(exportConfiguration.WorksheetName);
+                    var currentDataRow =
+                        CalculateDataHeaderRow(exportConfiguration.RenderTitle, exportConfiguration.RenderSubTitle);
+
+                    //Render the data headers first, to establish the range of the sheet, plus column names
+                    var headerNames = new List<string>();
+
+                    //Run headers
+                    var properties = TypeDescriptor.GetProperties(exportConfiguration.DataType);
+                    for (var i = 0; i < properties.Count; i++)
+                    {
+                        sheet.Cells[currentDataRow, i + 1].Value = properties[i].DisplayName;
+                        headerNames.Add(properties[i].DisplayName);
+
+                        //Handle formats
+                        if (properties[i].Attributes.Count <= 0) continue;
+                        foreach (var attribute in properties[i].Attributes)
+                        {
+                            if (!(attribute is SpreadsheetColumnFormatAttribute detail))
+                                continue;
+
+                            sheet.Column(i + 1).Style.Numberformat.Format = GetFormatSpecifier(detail.Format);
+                            break;
+                        }
+                    }
+
+                    //Style the header cells
+                    using (var headerRange = sheet.Cells[currentDataRow, 1, currentDataRow, headerNames.Count])
+                    {
+                        headerRange.Style.Font.Bold = true;
+                        headerRange.Style.WrapText = true;
+                    }
+
+                    //Set title
+                    if (exportConfiguration.RenderTitle)
+                    {
+                        //always at row 1
+                        using (var titleRange = sheet.Cells[1, 1, 1, headerNames.Count])
+                        {
+                            titleRange.Merge = true;
+                            titleRange.Value = exportConfiguration.DocumentTitle;
+                            titleRange.Style.Font.Bold = true;
+                            titleRange.Style.Font.Size = 14;
+                        }
+                    }
+
+                    //Set Sub-Title
+                    if (exportConfiguration.RenderSubTitle)
+                    {
+                        var subHeaderRow = currentDataRow - 1; //Just before header
+                        using (var titleRange = sheet.Cells[subHeaderRow, 1, subHeaderRow, headerNames.Count])
+                        {
+                            titleRange.Merge = true;
+                            titleRange.Value = exportConfiguration.DocumentSubTitle;
+                            titleRange.Style.Font.Bold = true;
+                            titleRange.Style.Font.Size = 12;
+                        }
+                    }
+
+                    //Load data
+                    foreach (var item in exportConfiguration.ExportData)
+                    {
+                        currentDataRow++; //Increment current row
+                        for (var p = 0; p < properties.Count; p++)
+                        {
+                            sheet.Cells[currentDataRow, p + 1].Value = properties[p].GetValue(item);
+                        }
+                    }
+
+                    //Auto Fit
+                    sheet.Cells[sheet.Dimension.Address].AutoFitColumns();
+                }
+
+                //Return the final object
+                return package.GetAsByteArray();
+            }
+        }
+
         /// <summary>
         ///     Creates a single worksheet document using the provided configuration information
         /// </summary>
