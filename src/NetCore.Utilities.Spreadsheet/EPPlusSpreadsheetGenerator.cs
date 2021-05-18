e202c6f41f408f21b2389db5b1d8cea3601ab375
Mitchel Sellers <msellers@Iowacomputergurus.com>
2021-05-18 00:31:45 -0500

    Data flowing properly.  Missing Auto-Size

--- src/NetCore.Utilities.Spreadsheet/EPPlusSpreadsheetGenerator.cs
+++ src/NetCore.Utilities.Spreadsheet/EPPlusSpreadsheetGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
@@ -107,11 +108,26 @@ namespace ICG.NetCore.Utilities.Spreadsheet
             }
         }
 
+        /// <summary>
+        /// Internal helper for tracking font-style indexes
+        /// </summary>
         private enum FontStyleIndex
         {
+            /// <summary>
+            /// Default cell text
+            /// </summary>
             Default = 0,
+            /// <summary>
+            /// Document header formatting
+            /// </summary>
             Header = 1,
+            /// <summary>
+            /// Document sub-header formatting
+            /// </summary>
             SubHeader = 2,
+            /// <summary>
+            /// Headers for actual data.
+            /// </summary>
             DataHeader = 3
         }
 
@@ -168,8 +184,7 @@ namespace ICG.NetCore.Utilities.Spreadsheet
                 var stylesPart = spreadsheetDocument.WorkbookPart.AddNewPart<WorkbookStylesPart>();
                 stylesPart.Stylesheet = new Stylesheet();
                 // blank font list
-                stylesPart.Stylesheet.Fonts = new Fonts();
-                stylesPart.Stylesheet.Fonts.Count = 4;
+                stylesPart.Stylesheet.Fonts = new Fonts {Count = 4};
                 stylesPart.Stylesheet.Fonts.AppendChild(new Font()
                 {
                     FontSize = new FontSize { Val = 11 }
@@ -193,9 +208,13 @@ namespace ICG.NetCore.Utilities.Spreadsheet
                 stylesPart.Stylesheet.Fills = new Fills();
 
                 // create a solid red fill
-                var solidRed = new PatternFill() { PatternType = PatternValues.Solid };
-                solidRed.ForegroundColor = new ForegroundColor { Rgb = HexBinaryValue.FromString("FFFF0000") }; // red fill
-                solidRed.BackgroundColor = new BackgroundColor { Indexed = 64 };
+                var solidRed = new PatternFill
+                {
+                    PatternType = PatternValues.Solid,
+                    ForegroundColor = new ForegroundColor {Rgb = HexBinaryValue.FromString("FFFF0000")},
+                    BackgroundColor = new BackgroundColor {Indexed = 64}
+                };
+                // red fill
 
                 stylesPart.Stylesheet.Fills.AppendChild(new Fill { PatternFill = new PatternFill { PatternType = PatternValues.None } }); // required, reserved by Excel
                 stylesPart.Stylesheet.Fills.AppendChild(new Fill { PatternFill = new PatternFill { PatternType = PatternValues.Gray125 } }); // required, reserved by Excel
@@ -214,9 +233,9 @@ namespace ICG.NetCore.Utilities.Spreadsheet
 
                 // cell format list
                 stylesPart.Stylesheet.CellFormats = new CellFormats();
-                // empty one for index 0, seems to be required
+                
                 stylesPart.Stylesheet.CellFormats.AppendChild(new CellFormat()); //Default
-                // cell format references style format 0, font 0, border 0, fill 2 and applies the fill
+
                 //Header
                 stylesPart.Stylesheet.CellFormats.AppendChild(new CellFormat { FormatId = 0, FontId = 1, BorderId = 0, FillId = 0 });//.AppendChild(new Alignment { Horizontal = HorizontalAlignmentValues.Center });
                 //Sub-header
@@ -226,11 +245,9 @@ namespace ICG.NetCore.Utilities.Spreadsheet
                 stylesPart.Stylesheet.CellFormats.AppendChild(new CellFormat
                 { FormatId = 0, FontId = 3, BorderId = 0, FillId = 0 });
 
-                stylesPart.Stylesheet.CellFormats.Count = 3;
-
+                stylesPart.Stylesheet.CellFormats.Count = 4;
                 stylesPart.Stylesheet.Save();
-
-
+                
 
                 //Build out our sheet information
                 UInt32 currentRow = 1;
@@ -267,22 +284,20 @@ namespace ICG.NetCore.Utilities.Spreadsheet
                 }
 
                 //Run data headers
-                var headerNames = new List<string>();
                 //TODO: AUto Size - https://stackoverflow.com/questions/18268620/openxml-auto-size-column-width-in-excel
                 //Run headers
-                var properties = TypeDescriptor.GetProperties(typeof(T));
+                var properties = typeof(T).GetProperties();
+                var headerProperties = TypeDescriptor.GetProperties(typeof(T));
                 var headerRow = new Row { RowIndex = currentRow };
-                for (var i = 0; i < properties.Count; i++)
+                foreach(PropertyDescriptor prop in headerProperties)
                 {
                     var headerCell = new Cell
                     {
-                        CellValue = new CellValue(properties[i].DisplayName),
+                        CellValue = new CellValue(prop.DisplayName),
                         DataType = CellValues.String,
                         StyleIndex = (int)FontStyleIndex.DataHeader
                     };
                     headerRow.Append(headerCell);
-                    headerNames.Add(properties[i].DisplayName);
-
 
                     //Handle formats
                     //if (properties[i].Attributes.Count <= 0) continue;
@@ -302,11 +317,12 @@ namespace ICG.NetCore.Utilities.Spreadsheet
                 foreach (var item in exportConfiguration.ExportData)
                 {
                     var dataRow = new Row {RowIndex = currentRow};
-                    for (var p = 0; p < properties.Count; p++)
+                    foreach (var prop in properties)
                     {
+                        var itemValue = prop.GetValue(item);
                         var dataCell = new Cell
                         {
-                            CellValue = new CellValue(properties[0].GetValue(item).ToString()),
+                            CellValue = new CellValue(itemValue?.ToString()),
                             DataType = CellValues.String
                         };
                         dataRow.Append(dataCell);
@@ -316,22 +332,16 @@ namespace ICG.NetCore.Utilities.Spreadsheet
                     currentRow++;
                 }
 
+                //Auto-size
+                Columns columns = AutoSize(data);
+                //worksheetPart.Worksheet.Append(columns);
+
                 workbookPart.Workbook.Save();
                 spreadsheetDocument.Close();
 
                 //Return the bytearray
                 documentStream.Seek(0, SeekOrigin.Begin);
                 return documentStream.ToArray();
-
-
-
-
-                
-
-                ////Auto Fit
-                //sheet.Cells[sheet.Dimension.Address].AutoFitColumns();
-
-                //return package.GetAsByteArray();
             }
         }
 
@@ -368,6 +378,82 @@ namespace ICG.NetCore.Utilities.Spreadsheet
                 default:
                     return requestedFormat;
             }
+        }
+
+        private Columns AutoSize(SheetData sheetData)
+        {
+            var maxColWidth = GetMaxCharacterWidth(sheetData);
+
+            Columns columns = new Columns();
+            //this is the width of my font - yours may be different
+            double maxWidth = 7;
+            foreach (var item in maxColWidth)
+            {
+                //width = Truncate([{Number of Characters} * {Maximum Digit Width} + {5 pixel padding}]/{Maximum Digit Width}*256)/256
+                double width = Math.Truncate((item.Value * maxWidth + 5) / maxWidth * 256) / 256;
+
+                //pixels=Truncate(((256 * {width} + Truncate(128/{Maximum Digit Width}))/256)*{Maximum Digit Width})
+                double pixels = Math.Truncate(((256 * width + Math.Truncate(128 / maxWidth)) / 256) * maxWidth);
+
+                //character width=Truncate(({pixels}-5)/{Maximum Digit Width} * 100+0.5)/100
+                double charWidth = Math.Truncate((pixels - 5) / maxWidth * 100 + 0.5) / 100;
+
+                Column col = new Column() { BestFit = true, Min = (UInt32)(item.Key + 1), Max = (UInt32)(item.Key + 1), CustomWidth = true, Width = (DoubleValue)width };
+                columns.Append(col);
+            }
+
+            return columns;
+        }
+
+
+        private Dictionary<int, int> GetMaxCharacterWidth(SheetData sheetData)
+        {
+            //iterate over all cells getting a max char value for each column
+            Dictionary<int, int> maxColWidth = new Dictionary<int, int>();
+            var rows = sheetData.Elements<Row>();
+            UInt32[] numberStyles = new UInt32[] { 5, 6, 7, 8 }; //styles that will add extra chars
+            UInt32[] boldStyles = new UInt32[] { 1, 2, 3, 4, 6, 7, 8 }; //styles that will bold
+            foreach (var r in rows)
+            {
+                var cells = r.Elements<Cell>().ToArray();
+
+                //using cell index as my column
+                for (int i = 0; i < cells.Length; i++)
+                {
+                    var cell = cells[i];
+                    var cellValue = cell.CellValue == null ? string.Empty : cell.CellValue.InnerText;
+                    var cellTextLength = cellValue.Length;
+
+                    if (cell.StyleIndex != null && numberStyles.Contains(cell.StyleIndex))
+                    {
+                        int thousandCount = (int)Math.Truncate((double)cellTextLength / 4);
+
+                        //add 3 for '.00' 
+                        cellTextLength += (3 + thousandCount);
+                    }
+
+                    if (cell.StyleIndex != null && boldStyles.Contains(cell.StyleIndex))
+                    {
+                        //add an extra char for bold - not 100% acurate but good enough for what i need.
+                        cellTextLength += 1;
+                    }
+
+                    if (maxColWidth.ContainsKey(i))
+                    {
+                        var current = maxColWidth[i];
+                        if (cellTextLength > current)
+                        {
+                            maxColWidth[i] = cellTextLength;
+                        }
+                    }
+                    else
+                    {
+                        maxColWidth.Add(i, cellTextLength);
+                    }
+                }
+            }
+
+            return maxColWidth;
         }
     }
 }
